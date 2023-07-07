8491d9db3540e41ceaca0060fed9fdfa835c15a4
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-07 00:03:50 -0400

    Add ability to loop a simulated RPS game 5 times

---
+++ script.js
@@ -0,0 +1,40 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    if (computerChoice === 1){
+        return 'ROCK';
+    } else if (computerChoice === 2){
+        return 'PAPER';
+    } else if (computerChoice === 3){
+        return 'SCISSORS';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+
+    if(playerSelection === computerSelection){
+        return `Its a tie!`;
+    }
+
+    if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'
+    || playerSelection === 'PAPER' && computerSelection === 'ROCK'
+    || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    if(playerSelection === 'ROCK' && computerSelection === 'PAPER'
+    || playerSelection === 'PAPER' && computerSelection === 'SCISSORS'
+    || playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let playerSelection = "rock";
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+game();
