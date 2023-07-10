49e6b8b22db420c25e08e7e34bcca31e6e90b4eb
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-09 20:19:00 -0400

    Complete functional UI for RPS game including score and buttons

--- script.js
+++ script.js
@@ -1,54 +1,75 @@
-
+const buttons = document.querySelectorAll('button');
 let playerScore = 0;
 let computerScore = 0;
 
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    });
+}
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     if (computerChoice === 1){
-        return 'ROCK';
+        return 'Rock';
     } else if (computerChoice === 2){
-        return 'PAPER';
+        return 'Paper';
     } else if (computerChoice === 3){
-        return 'SCISSORS';
+        return 'Scissors';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
-    if(playerSelection === computerSelection){
-        return `Draw!`;
+    if (playerSelection === computerSelection) {
+        return 'Draw!';
     }
 
-    if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'
-    || playerSelection === 'PAPER' && computerSelection === 'ROCK'
-    || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+    if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')
+    ) {
         playerScore++;
+        score.textContent = `${playerScore} - ${computerScore}`;
+        if(playerScore === 5){
+            disableButtons();
+            return `You win!`; 
+        }
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
 
-    if(playerSelection === 'ROCK' && computerSelection === 'PAPER'
-    || playerSelection === 'PAPER' && computerSelection === 'SCISSORS'
-    || playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+    if (
+        (playerSelection === 'Rock' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Rock')
+    ) {
         computerScore++;
+        score.textContent = `${playerScore} - ${computerScore}`;
+        if(computerScore === 5){
+            disableButtons();
+            return `You lose!`; 
+        }
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-function game(){
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
 
-    if(playerScore > computerScore){
-        console.log('The player wins!');
-    } else if (computerScore > playerScore) {
-        console.log('The computer wins!');
-    } else {
-        console.log('Its a tie!');
-    }
-}
-game();
+    const rockBtn = document.getElementById('rock');
+    const paperBtn = document.getElementById('paper');
+    const scissorsBtn = document.getElementById('scissors');
+    const results = document.getElementById('results')
+
+    rockBtn.addEventListener('click', () => results.textContent = (playRound('rock', getComputerChoice())));
+    paperBtn.addEventListener('click', () => results.textContent =(playRound('paper', getComputerChoice())));
+    scissorsBtn.addEventListener('click', () => results.textContent =(playRound('scissors', getComputerChoice())));    
+
+    const container = document.querySelector('.player-button-container');
+    const score = document.createElement('p');
+    score.classList.add('score');
+    score.textContent = `${playerScore} - ${computerScore}`;
+    score.style.color = 'red';
+
+    container.appendChild(score);
