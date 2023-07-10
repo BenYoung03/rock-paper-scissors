58b5f7b47e4be2ab3f1ee90491726bbeef60fa32
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-10 14:45:46 -0400

    Improve readability of code by adding comments

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const buttons = document.querySelectorAll('.game');
+//Global variables
 let playerScore = 0;
 let computerScore = 0;
 let compWin = false;
 let computerChoice = 0;
 
+//Reset button
 const resetBtn = document.querySelector('#reset');
 
 //refresh page for new game
 resetBtn.addEventListener('click',() => location.reload());
 
+//Function generates a computer choice (one of rock, paper or scissors)
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1){
@@ -20,6 +22,7 @@ function getComputerChoice() {
     }
 }
 
+//Changes the colour of the computer button on round completetion depending on choice, and win or loss
 function changeCompButtonColour() {
     if (computerChoice === 1){
         if(compWin === true) compRockBtn.style.backgroundColor = 'green'; 
@@ -35,13 +38,15 @@ function changeCompButtonColour() {
     }
 }
 
+//Plays a round
 function playRound(playerSelection, computerSelection) {
 
+    //Resets all buttons colours
     buttons.forEach(elem => {
         elem.style.backgroundColor = 'white';
     });
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
+    //Runs if a draw occurs
     if (playerSelection === computerSelection) {
         compWin = true;
         if(playerSelection === 'Rock'){
@@ -57,15 +62,18 @@ function playRound(playerSelection, computerSelection) {
         return 'Draw!';
     }
 
+    //If the player beats the computer
     if (
         (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
         (playerSelection === 'Paper' && computerSelection === 'Rock') ||
         (playerSelection === 'Scissors' && computerSelection === 'Paper')
     ) {
+        //Increment player score and update on screen text
         playerScore++;
         compWin = false;
         player.textContent = `Score: ${playerScore}`;
 
+        //Selects the correct player choice and updates button colours
         if(playerSelection === 'Rock'){
             rockBtn.style.backgroundColor = 'green';
             changeCompButtonColour();
@@ -77,6 +85,7 @@ function playRound(playerSelection, computerSelection) {
             changeCompButtonColour();
         }
 
+        //Ends if player score is equal to 5 resulting in a game win
         if(playerScore === 5){
             buttons.forEach(elem => {
                 elem.disabled = true;
@@ -86,18 +95,22 @@ function playRound(playerSelection, computerSelection) {
             audio.play();
             return `You win!`; 
         }
+        //Updates result text with the correct round results
         return `You win! ${playerSelection} beats ${computerSelection}`;
     }
 
+    //If player loses to computer
     if (
         (playerSelection === 'Rock' && computerSelection === 'Paper') ||
         (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
         (playerSelection === 'Scissors' && computerSelection === 'Rock')
     ) {
+        //Increment computer score and update on screen text
         computerScore++;
         compWin = true;
         computer.textContent = `Score: ${computerScore}`;
 
+        //Selects the correct player choice and updates button colours
         if(playerSelection === 'Rock'){
             rockBtn.style.backgroundColor = 'red';
             changeCompButtonColour();
@@ -109,6 +122,7 @@ function playRound(playerSelection, computerSelection) {
             changeCompButtonColour();
         }
 
+        //Ends if computer score is equal to 5 resulting in a game loss
         if(computerScore === 5){
             buttons.forEach(elem => {
                 elem.disabled = true;
@@ -118,6 +132,7 @@ function playRound(playerSelection, computerSelection) {
             audio.play();
             return `You lose!`; 
         }
+        //Updates result text with the correct round results
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
@@ -131,29 +146,31 @@ const compRockBtn = document.getElementById('rock-computer');
 const compPaperBtn = document.getElementById('paper-computer');
 const compScissorsBtn = document.getElementById('scissors-computer');
 const results = document.getElementById('results')
+const buttons = document.querySelectorAll('.game');
 
-
+//Event listeners for the player buttons that send the correct move to the playRound function and plays audio on click
 rockBtn.addEventListener('click', function() {
-    results.textContent = (playRound('rock', getComputerChoice()));
+    results.textContent = (playRound('Rock', getComputerChoice()));
     const audio = document.getElementById('click');
     audio.currentTime = 0;
     audio.play();
 });
 
 paperBtn.addEventListener('click', function() {
-    results.textContent = (playRound('paper', getComputerChoice()));
+    results.textContent = (playRound('Paper', getComputerChoice()));
     const audio = document.getElementById('click');
     audio.currentTime = 0;
     audio.play();
 });
 
 scissorsBtn.addEventListener('click', function() {
-    results.textContent = (playRound('scissors', getComputerChoice()));
+    results.textContent = (playRound('Scissors', getComputerChoice()));
     const audio = document.getElementById('click');
     audio.currentTime = 0;
     audio.play();
 });
 
+//Makes the player score text a child of the playerContainer
 const playerContainer = document.querySelector('.player-button-container');
 const player = document.createElement('p');
 player.classList.add('score');
@@ -162,6 +179,7 @@ player.style.color = 'red';
 
 playerContainer.appendChild(player);
 
+//Makes the computer score text a child of the computerContainer
 const computerContainer = document.querySelector('.computer-button-container');
 const computer = document.createElement('p');
 computer.classList.add('score');
